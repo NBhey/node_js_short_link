fbf336defbf13ad2ec8cd480205122a88e79a3f4
NBhey <122431282+NBhey@users.noreply.github.com>
2026-07-30 21:38:42 +0300

    study: понять, что ос принимает соединение, держит их очередь. 	libuv же берет из ядра и раскладывает по фащам цикла

--- async.js
+++ async.js
@@ -1,11 +1,21 @@
 const http = require("node:http");
 
+const startServer = Date.now();
+
 const server = http.createServer((incomingMessage, serverResponse) => {
+  const url = incomingMessage.url;
+  console.log("старт обработки" + url, Date.now() - startServer);
   const start = Date.now();
 
   while (Date.now() - start < 5000) {}
 
+  console.log("конец" + url, Date.now() - startServer);
   serverResponse.end();
 });
 
+server.on("connection", () => {
+  console.log("соединение", Date.now() - startServer);
+});
+
 server.listen(4040, "127.0.0.1");
+console.log("старт сервера", Date.now() - startServer);
