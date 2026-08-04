ac6ebfcf621d43ccb311c9b5e7ebb240db9b7575
NBhey <122431282+NBhey@users.noreply.github.com>
2026-08-04 19:19:12 +0300

    feat: добавить роут json

--- express.mjs
+++ express.mjs
@@ -7,6 +7,10 @@ app.get("/", (request, response) => {
   response.send("Hello Express!");
 });
 
+app.get("/json", (request, response) => {
+  response.send({ field: "Hello world" });
+});
+
 app.listen(PORT, () => {
   console.log("Приложение запущено на " + PORT + " порту");
 });
